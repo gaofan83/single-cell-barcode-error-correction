16f1c53e070694a8f7231ccc450be9ee710b599a
Fan Gao <gaofan83@hotmail.com>
2020-05-08 19:00:16 -0700

    Update reads_per_cell.sh

--- reads_per_cell.sh
+++ reads_per_cell.sh
@@ -1,7 +1,7 @@
 #Extract cell barcodes from raw R1 reads
 zcat raw_R1.fastq.gz | awk '{if(NR%4==2) print substr($1,1,16)}' > bc_raw.txt
 
-#Creat a filtered cell barcode file barcodes.tsv, and search them in raw data
+#Creat a filtered cell barcode file barcodes.tsv, and search the filtered barcodes in raw data
 awk 'NR==FNR{Arr[$1]++;next} ($1 in Arr){print $1}' barcodes.tsv bc_raw.txt > bc_match.txt
 
 #Count occurrence of cell barcodes
